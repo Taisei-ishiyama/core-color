9262e5a3f34ba4563b4e9570248da5386a8ef31c
taisei.ishiyama <i.taisei0411@gmail.com>
2018-12-26 05:32:43 +0000

    for heroku re

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -3,8 +3,8 @@ class Inquiry
   
   attr_accessor :inquiry_type, :first_name, :name, :first_name_kana, :name_kana, :email, :phone_number,:message 
   
-  # validates :inquiry_type, :presence => {:message => 'お問い合わせ項目を選択してください'}
-  # validates :name, :presence => {:message => '名前を入力してください'}
-  # validates :email, :presence => {:message => 'メールアドレスを入力してください'}
-  # validates :message, :presence => {:message => 'お問い合わせ内容を入力してください'}
+  validates :inquiry_type, :presence => {:message => 'お問い合わせ項目を選択してください'}
+  validates :name, :presence => {:message => '名前を入力してください'}
+  validates :email, :presence => {:message => 'メールアドレスを入力してください'}
+  validates :message, :presence => {:message => 'お問い合わせ内容を入力してください'}
 end
